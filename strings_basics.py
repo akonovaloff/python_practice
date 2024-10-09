a876477fab68caf6183efffec0c77dad46ab2c68
Andrei Konovalov <daredevilenok91@gmail.com>
2024-10-09 18:40:50 +0400

    Update strings_basics.py

--- strings_basics.py
+++ strings_basics.py
@@ -1,27 +1,29 @@
 import argparse
+import click
 
-
-def string_splitter(string_to_split, split_symbol):
+def string_splitter(string_to_split: str, split_symbol: str) -> list:
     """
     :param string_to_split: String to separate
     :param split_symbol: Separator to separate by
     :return: List of separated substrings
     """
-    return  # Add code here to split 'string_to_split' with 'split_symbol'
+    # Add code here to split 'string_to_split' with 'split_symbol'
+    return string_to_split.split(split_symbol)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description='Split "String" by "Separator"')
 
     # Add required info form task here
-    parser.add_argument('-string', action='store', required=True)
+    parser.add_argument("-string", action="store", required=True)
 
     # Add required info form task here
-    parser.add_argument('-separator', action='store', required=True)
+    parser.add_argument("-separator", action="store", required=True)
 
     args = parser.parse_args()
     income_string = args.string
     separator_symbol = args.separator
 
     # Call function "string_splitter" with parameters to print result
-    print()
+    print(string_splitter(income_string, separator_symbol))
